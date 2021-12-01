5c23225d760e06705a20a4847f77dc5e0b8daeec
Faina-Murashko <fainka18@gmail.com>
2021-12-01 20:00:57 +0300

    Initial commit

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+        WebDriver driver;
+
+        @BeforeTest
+        public void SetUp(){
+            System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--start-maximized");
+            driver = new ChromeDriver(options);
+            driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+
+        }
+
+        @AfterTest
+        public void tearDown(){
+            driver.quit();
+
+        }
+    }
+
