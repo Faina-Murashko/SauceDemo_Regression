5c9e26a0086163f83b6be8afa80297cdb5193428
Faina-Murashko <fainka18@gmail.com>
2021-12-06 22:01:10 +0300

    Create refactoring

--- src/test/java/page/LoginPage.java
+++ src/test/java/page/LoginPage.java
@@ -1,13 +1,18 @@
-package tests;
+package page;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class LoginPages {
+public class LoginPage {
     WebDriver driver;
     public static final By USERNAME_INPUT = By.name("user-name");
     public static final By PASSWORD_INPUT = By.id("password");
     public static final By LOGIN_BUTTON = By.name("login-button");
+
+    public LoginPage(WebDriver driver) {
+
+        this.driver = driver;
+    }
 
 
     //public static final By ERROR_MASSAGE = By.name("user-name");
@@ -17,4 +22,10 @@ public class LoginPages {
         driver.findElement(PASSWORD_INPUT).sendKeys(password);
         driver.findElement(LOGIN_BUTTON).click();
     }
+
+    public void open() {
+        driver.get("https://www.saucedemo.com/");
+    }
+
+
 }
--- src/test/java/tests/InventoryAndCartTesting.java
+++ src/test/java/tests/InventoryAndCartTesting.java
@@ -1,38 +1,38 @@
+package tests;
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
-public class LoginTest extends BaseTest {
+public class InventoryAndCartTesting extends BaseTest {
     @Test
-    public void loginTesting (){
-        driver.get("https://www.saucedemo.com/");
-        WebElement inputUserName = driver.findElement(By.name("user-name"));
-        inputUserName.sendKeys("standard_user");
-        WebElement inputUserPassword = driver.findElement(By.id("password"));
-        inputUserPassword.sendKeys("secret_sauce");
-        driver.findElement(By.name("login-button")).click();
+    public void variableInventoryAndCart() throws InterruptedException {
+        loginPage.open();
+        loginPage.login("standard_user", "secret_sauce");
+        Thread.sleep(2000);
 
-        driver.findElement(By.id("item_4_title_link")).click();
+
+
+
+    /*driver.findElement(By.id("item_4_title_link")).click();
         driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
         driver.navigate().back();
         driver.findElement(By.className("shopping_cart_link")).click();
         driver.findElement(By.className("inventory_item_price"));
-        assertEquals(29.99, 29.99);
+        assertTrue(29.99);
         driver.navigate().back();
 
         driver.findElement(By.id("item_2_title_link")).click();
         driver.findElement(By.id("add-to-cart-sauce-labs-onesie")).click();
         driver.findElement(By.className("shopping_cart_link")).click();
         driver.findElement(By.className("inventory_item_price"));
-        assertEquals(29.99,29.99);
-        assertEquals(7.99, 7.99);
+        assertTrue(7.99);
         driver.findElement(By.id("checkout")).click();
+}
 
-
-
-
-
+    private void assertTrue(double s) {
+    }*/
     }
 }
